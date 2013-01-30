45418151f85f4fe2400acebbdf444bef63ebeb44
Brittany Yoon <binna.yoon@gmail.com>
2013-01-30 10:18:32 -0800

    Done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,18 @@
 class Artist < ActiveRecord::Base
+  attr_accessible :first_name, :last_name, :band_id
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  belongs_to :band
 
+  has_many :authored_songs, :class_name => 'Song', :foreign_key => :author_id
+  has_many :performed_songs, :through => :band, :source => :recordings
+
+  def self.authored_song_hash
+    artists = Artist.includes(:authored_songs)
+    hash = {}
+    artists.each do |artist|
+      hash[artist] = artist.authored_songs
+    end
+    hash
+  end
 end
--- db/migrate/20130130171607_create_song_and_recording.rb
+++ db/migrate/20130130171607_create_song_and_recording.rb
@@ -1,7 +1,14 @@
 class CreateSongAndRecording < ActiveRecord::Migration
-  def up
+  def change
+    create_table :songs do |t|
+      t.string :song_name
+      t.string :author_id
+    end
+
+    create_table :recordings do |t|
+      t.integer :song_id
+      t.integer :band_id
+    end
   end
 
-  def down
-  end
 end
